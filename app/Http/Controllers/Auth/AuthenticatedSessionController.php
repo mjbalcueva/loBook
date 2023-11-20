c2c6d46860e6ade2d1fb44531155a1b0fca06e6b
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-11-20 19:20:50 +0800

    Fix capitalization in Inertia render paths

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -19,7 +19,7 @@ class AuthenticatedSessionController extends Controller
    */
   public function create(): Response
   {
-    return Inertia::render('Auth/login', [
+    return Inertia::render('Auth/Login', [
       'canResetPassword' => Route::has('password.request'),
       'status' => session('status'),
     ]);
--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -21,7 +21,7 @@ class RegisteredUserController extends Controller
    */
   public function create(): Response
   {
-    return Inertia::render('Auth/register');
+    return Inertia::render('Auth/Register');
   }
 
   /**
