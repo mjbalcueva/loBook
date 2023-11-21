045e3a093b9aa14513cebb89464e76572857a1e2
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-11-21 23:30:51 +0800

    Refactor VerifyEmail component and update layout

--- resources/js/Pages/Auth/VerifyEmail.tsx
+++ resources/js/Pages/Auth/VerifyEmail.tsx
@@ -1,46 +1,72 @@
-import GuestLayout from '@/Layouts/GuestLayout';
-import PrimaryButton from '@/Components/PrimaryButton';
-import { Head, Link, useForm } from '@inertiajs/react';
-import { FormEventHandler } from 'react';
+import { Link, useForm } from "@inertiajs/react"
+import { FormEventHandler } from "react"
 
-export default function VerifyEmail({ status }: { status?: string }) {
-    const { post, processing } = useForm({});
+import { Form } from "@/Components/form-inertia"
+import { Badge } from "@/Components/ui/badge"
+import { buttonVariants } from "@/Components/ui/button"
+import { AuthLayout } from "@/Layouts/auth-layout"
 
-    const submit: FormEventHandler = (e) => {
-        e.preventDefault();
-
-        post(route('verification.send'));
-    };
-
-    return (
-        <GuestLayout>
-            <Head title="Email Verification" />
-
-            <div className="mb-4 text-sm text-gray-600 dark:text-gray-400">
-                Thanks for signing up! Before getting started, could you verify your email address by clicking on the
-                link we just emailed to you? If you didn't receive the email, we will gladly send you another.
-            </div>
-
-            {status === 'verification-link-sent' && (
-                <div className="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-                    A new verification link has been sent to the email address you provided during registration.
-                </div>
-            )}
-
-            <form onSubmit={submit}>
-                <div className="mt-4 flex items-center justify-between">
-                    <PrimaryButton disabled={processing}>Resend Verification Email</PrimaryButton>
-
-                    <Link
-                        href={route('logout')}
-                        method="post"
-                        as="button"
-                        className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                    >
-                        Log Out
-                    </Link>
-                </div>
-            </form>
-        </GuestLayout>
-    );
+interface Props {
+	status?: string
 }
+
+const VerifyEmail = ({ status }: Props) => {
+	const { post, processing } = useForm({})
+
+	const onSubmit: FormEventHandler = (e) => {
+		e.preventDefault()
+
+		post(route("verification.send"))
+	}
+
+	return (
+		<Form
+			onSubmit={onSubmit}
+			className="space-y-6"
+		>
+			{status === "verification-link-sent" && (
+				<p className="text-sm text-muted-foreground">
+					A new verification link has been sent to the email address you
+					provided during registration.
+					<Badge
+						variant={"outline"}
+						className="ml-2 text-secondary-foreground"
+					>
+						link sent
+					</Badge>
+				</p>
+			)}
+
+			<div className="flex items-center justify-between">
+				<Form.Action
+					processing={processing}
+					onProcess="Sending..."
+					onSuccess="Sent!"
+				>
+					Resend Verification Email
+				</Form.Action>
+
+				<Link
+					href={route("logout")}
+					method="post"
+					as="button"
+					className={buttonVariants({ variant: "link" })}
+				>
+					Log Out
+				</Link>
+			</div>
+		</Form>
+	)
+}
+
+VerifyEmail.layout = (page: React.ReactNode) => (
+	<AuthLayout
+		pageTitle="Verify Email"
+		cardTitle="Thanks for signing up!"
+		cardDescription="Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another."
+	>
+		{page}
+	</AuthLayout>
+)
+
+export default VerifyEmail
