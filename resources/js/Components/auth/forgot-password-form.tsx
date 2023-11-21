7e33be8cbacd827a534f1cc94c5c9c33564e8669
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-11-21 15:57:57 +0800

    Add forgot password form component

---
+++ resources/js/Components/auth/forgot-password-form.tsx
@@ -0,0 +1,80 @@
+import { Link, useForm } from "@inertiajs/react"
+import { FC, FormEventHandler, useEffect } from "react"
+
+import {
+	Form,
+	FormItem,
+	FormMessage,
+	FormProcessing,
+} from "@/Components/form-inertia"
+import { Button } from "@/Components/ui/button"
+import { Input } from "@/Components/ui/input"
+import { Label } from "@/Components/ui/label"
+
+interface Props {
+	status?: string
+}
+
+const ForgotPasswordForm: FC<Props> = ({ status }) => {
+	const { data, setData, post, processing, errors, recentlySuccessful } =
+		useForm({
+			email: "",
+		})
+
+	const onSubmit: FormEventHandler = (e) => {
+		e.preventDefault()
+
+		post(route("password.email"))
+	}
+
+	return (
+		<Form>
+			<form
+				onSubmit={onSubmit}
+				className="space-y-2"
+			>
+				<FormItem>
+					<Label htmlFor="email">Email</Label>
+					<Input
+						id="email"
+						type="email"
+						value={data.email}
+						onChange={(e) => setData("email", e.target.value)}
+						placeholder="Your Email"
+						autoComplete="username"
+					/>
+					<FormMessage
+						message={errors.email}
+						className="mt-2 text-sm font-medium text-destructive"
+					/>
+					<FormMessage
+						message={status}
+						className="mt-2 text-sm font-medium text-green-600"
+					/>
+					{status && (
+						<div className="mb-4 text-sm font-medium text-green-600">
+							{status}
+						</div>
+					)}
+				</FormItem>
+
+				<div className="mt-4 flex items-center justify-end">
+					<Button
+						disabled={processing}
+						type="submit"
+					>
+						<FormProcessing
+							processing={processing}
+							recentlySuccessful={recentlySuccessful}
+							textDefault="Email Password Reset Link"
+							textProcessing="Sending..."
+							textSuccess="Sent!"
+						/>
+					</Button>
+				</div>
+			</form>
+		</Form>
+	)
+}
+
+export { ForgotPasswordForm }
