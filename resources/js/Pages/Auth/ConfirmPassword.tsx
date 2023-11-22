37b7c6809932fc3830f314203a7cef1a6eb35570
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-11-23 00:53:43 +0800

    Add ConfirmPasswordForm component

--- resources/js/Pages/Auth/ConfirmPassword.tsx
+++ resources/js/Pages/Auth/ConfirmPassword.tsx
@@ -1,54 +1,32 @@
-import { useForm } from "@inertiajs/react"
-import { FormEventHandler, useEffect } from "react"
-
-import { Form } from "@/Components/form-inertia"
+import ConfirmPasswordForm from "@/Components/auth/confirm-password-form"
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/Components/ui/card"
 import { AuthLayout } from "@/Layouts/auth-layout"
 
 const ConfirmPassword = () => {
-	const { data, setData, post, processing, errors, reset } = useForm({
-		password: "",
-	})
-	useEffect(() => {
-		return () => {
-			reset("password")
-		}
-	}, [])
-
-	const onSubmit: FormEventHandler = (e) => {
-		e.preventDefault()
-
-		post(route("password.confirm"))
-	}
-
 	return (
-		<Form
-			onSubmit={onSubmit}
-			className="space-y-4"
-		>
-			<Form.Input
-				label="Password"
-				password
-				value={data.password}
-				onChange={(e) => setData("password", e.target.value)}
-				placeholder="**********"
-				message={errors.password}
-			/>
-
-			<div className="flex items-center justify-end">
-				<Form.Action processing={processing}>Confirm</Form.Action>
-			</div>
-		</Form>
+		<Card>
+			<CardHeader>
+				<CardTitle>Confirm Password</CardTitle>
+				<CardDescription>
+					This is a secure area of the application. Please confirm your password
+					before continuing.
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<ConfirmPasswordForm />
+			</CardContent>
+		</Card>
 	)
 }
 
 ConfirmPassword.layout = (page: React.ReactNode) => (
-	<AuthLayout
-		pageTitle="Confirm Password"
-		cardTitle="Confirm password"
-		cardDescription="This is a secure area of the application. Please confirm your password before continuing."
-	>
-		{page}
-	</AuthLayout>
+	<AuthLayout pageTitle="Confirm Password">{page}</AuthLayout>
 )
 
 export default ConfirmPassword
