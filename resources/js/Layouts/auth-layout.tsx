e5548a95bd6b77c9f00956e08f091c39a2433133
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-11-22 21:20:21 +0800

    Add @radix-ui/react-aspect-ratio package

--- resources/js/Layouts/auth-layout.tsx
+++ resources/js/Layouts/auth-layout.tsx
@@ -1,8 +1,9 @@
-import { Head } from "@inertiajs/react"
-import { FC, PropsWithChildren } from "react"
+import { Head, Link } from "@inertiajs/react"
+import { PropsWithChildren } from "react"
 
 import { ActionCard } from "@/Components/action-card"
-import { ThemeProvider } from "@/Components/providers/theme-provider"
+import { AspectRatio } from "@/Components/ui/aspect-ratio"
+import { DefaultLayout } from "@/Layouts/default-layout"
 
 interface Props extends PropsWithChildren {
 	pageTitle: string
@@ -10,29 +11,61 @@ interface Props extends PropsWithChildren {
 	cardDescription: string
 }
 
-const AuthLayout: FC<Props> = ({
+const AuthLayout = ({
 	pageTitle,
 	cardTitle,
 	cardDescription,
 	children,
-}) => {
+}: Props) => {
 	return (
-		<ThemeProvider
-			defaultTheme="system"
-			storageKey="vite-ui-theme"
-		>
+		<DefaultLayout>
 			<Head title={pageTitle} />
-			<div className="flex min-h-screen items-center justify-center">
-				<div className="w-[500px]">
-					<ActionCard
-						title={cardTitle}
-						description={cardDescription}
+			<div className="grid min-h-screen grid-cols-1 overflow-hidden md:grid-cols-2">
+				<AspectRatio ratio={16 / 9}>
+					<img
+						src="/images/auth-layout.webp"
+						alt="Love"
+						className="absolute inset-0 object-cover"
+						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+					/>
+					<div className="absolute inset-0 bg-gradient-to-t from-background to-background/40 md:to-background/20" />
+					<Link
+						href="/"
+						className="absolute left-8 top-6 z-20 flex items-center text-lg font-bold tracking-tight"
 					>
-						{children}
-					</ActionCard>
-				</div>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							className="mr-2 h-6 w-6"
+							aria-hidden="true"
+						>
+							<path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+						</svg>
+						<span>loBook</span>
+					</Link>
+					<div className="absolute bottom-6 left-8 z-20 line-clamp-3 text-base">
+						&ldquo;This library has saved me countless hours of work and helped
+						me deliver stunning designs to my clients faster than ever
+						before.&rdquo;
+					</div>
+				</AspectRatio>
+				<main className="container absolute top-1/2 col-span-1 flex -translate-y-1/2 items-center md:static md:top-0 md:col-span-1 md:flex md:translate-y-0">
+					<div className="mx-auto w-[450px]">
+						<ActionCard
+							title={cardTitle}
+							description={cardDescription}
+						>
+							{children}
+						</ActionCard>
+					</div>
+				</main>
 			</div>
-		</ThemeProvider>
+		</DefaultLayout>
 	)
 }
 
