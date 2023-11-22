beaae7a7bc8c9e79875625e30478fd4ce3539ed7
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-11-23 00:57:57 +0800

    Add forgot password form component

--- resources/js/Pages/Auth/ForgotPassword.tsx
+++ resources/js/Pages/Auth/ForgotPassword.tsx
@@ -1,7 +1,11 @@
-import { useForm } from "@inertiajs/react"
-import { FormEventHandler } from "react"
-
-import { Form, FormMessage } from "@/Components/form-inertia"
+import ForgotPasswordForm from "@/Components/auth/forgot-password-form"
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/Components/ui/card"
 import { AuthLayout } from "@/Layouts/auth-layout"
 
 interface Props {
@@ -9,58 +13,24 @@ interface Props {
 }
 
 const ForgotPassword = ({ status }: Props) => {
-	const { data, setData, post, processing, errors, recentlySuccessful } =
-		useForm({
-			email: "",
-		})
-
-	const onSubmit: FormEventHandler = (e) => {
-		e.preventDefault()
-
-		post(route("password.email"))
-	}
-
 	return (
-		<Form
-			onSubmit={onSubmit}
-			className="space-y-4"
-		>
-			<Form.Input
-				label="Email"
-				type="email"
-				value={data.email}
-				onChange={(e) => setData("email", e.target.value)}
-				placeholder="juandelacruz@gmail.com"
-			>
-				<FormMessage message={errors.email} />
-				<FormMessage
-					success
-					message={status}
-				/>
-			</Form.Input>
-
-			<div className="mt-4 flex items-center justify-end">
-				<Form.Action
-					processing={processing}
-					recentlySuccessful={recentlySuccessful}
-					onProcess="Sending..."
-					onSuccess="Sent!"
-				>
-					Email Password Reset Link
-				</Form.Action>
-			</div>
-		</Form>
+		<Card>
+			<CardHeader>
+				<CardTitle>Forgot your password?</CardTitle>
+				<CardDescription>
+					No problem. Just let us know your email address and we will email you
+					a password reset link that will allow you to choose a new one.
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<ForgotPasswordForm status={status} />
+			</CardContent>
+		</Card>
 	)
 }
 
 ForgotPassword.layout = (page: React.ReactNode) => (
-	<AuthLayout
-		pageTitle="Forgot Password"
-		cardTitle="Forgot your password?"
-		cardDescription="No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one."
-	>
-		{page}
-	</AuthLayout>
+	<AuthLayout pageTitle="Forgot Password">{page}</AuthLayout>
 )
 
 export default ForgotPassword
