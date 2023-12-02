c7680684db645613fbf718af5dea525403de6623
sheeshnigami <93526143+Shinigami823@users.noreply.github.com>
2023-12-02 22:04:44 +0800

    help me plssssssssss with db

--- app/Http/Controllers/AddBookController.php
+++ app/Http/Controllers/AddBookController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class AddBook extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Uploads.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request){
+        
+    }
+
 }
