5b149d9801ad36a6d91da6a419501d5024d47688
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-12-02 19:20:36 +0800

    Update Uploaded page with "Add book" heading and reading journey message

--- resources/js/Pages/Uploaded.tsx
+++ resources/js/Pages/Uploaded.tsx
@@ -9,13 +9,18 @@ const Uploaded = () => {
 		<>
 			<Head title="Uploaded" />
 			<div className="my-4 flex items-center justify-between">
-				<h2 className="text-3xl font-bold tracking-tight">Uploaded</h2>
+				<h2 className="text-3xl font-bold tracking-tight">Add book</h2>
 				<div className="flex items-center space-x-2">
 					<Button className="text-sm">
 						<PlusCircle className="mr-2 h-5 w-5" />
 						Upload book
 					</Button>
 				</div>
+			</div>
+			<div className="flex min-h-[50vh] items-center justify-center rounded-md border">
+				<p className="select-none text-muted-foreground">
+					Embark on your reading journey...
+				</p>
 			</div>
 		</>
 	)
