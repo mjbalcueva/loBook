8807d7e84506419f8f045f349260cb1bd651f000
Nekochan <115055895+kuro-Nekko@users.noreply.github.com>
2023-12-02 20:32:39 +0800

    new additions to the table add chapters

--- database/migrations/2023_12_02_122254_create_chapters_adds_table.php
+++ database/migrations/2023_12_02_122254_create_chapters_adds_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('chapters_adds', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('content');
             $table->timestamps();
             $table->bigInteger('book_id');
         });
