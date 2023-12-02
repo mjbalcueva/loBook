eb60d7b04fe68609d7adbaa5835c223be2c70fa3
Nekochan <115055895+kuro-Nekko@users.noreply.github.com>
2023-12-02 20:26:34 +0800

    adding fields

--- database/migrations/2023_12_02_122254_create_chapters_adds_table.php
+++ database/migrations/2023_12_02_122254_create_chapters_adds_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('chapters_adds', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->timestamps();
+            $table->bigInteger('book_id');
         });
     }
 
