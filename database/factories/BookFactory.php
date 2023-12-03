7d71992766baca78b760b4e378a88f633afe66be
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-12-03 23:56:39 +0800

    Add BookFactory and ChapterFactory classes

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'title' => $this->faker->sentence(),
+      'author' => $this->faker->name(),
+      'description' => $this->faker->paragraph(),
+      'genre' => $this->faker->word(),
+      'cover' => $this->faker->imageUrl(),
+      'user_id' => User::factory(),
+    ];
+  }
+
+  /**
+   * Configure the model factory.
+   * 
+   * @return $this
+   */
+  public function configure(): static
+  {
+    return $this->afterCreating(function (Book $book) {
+      $book->chapters()->saveMany(ChapterFactory::new()->count(3)->make());
+    });
+  }
+}
---
+++ database/factories/ChapterFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Chapter>
+ */
+class ChapterFactory extends Factory
+{
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'title' => $this->faker->sentence(),
+      'content' => $this->faker->paragraph(),
+      'book_id' => Book::factory(),
+    ];
+  }
+}
