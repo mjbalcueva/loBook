99b7ad51978809e6c4481bf5e674d2af43344dbe
sheeshnigami <93526143+Shinigami823@users.noreply.github.com>
2023-12-04 21:13:09 +0800

    Updated UploadController

--- routes/web.php
+++ routes/web.php
@@ -46,9 +46,10 @@ Route::middleware('auth', 'verified')->group(function () {
   Route::get('/uploads', [UploadController::class, 'index'])->name('uploads.index');
   Route::get('/uploads/book', [UploadController::class, 'create'])->name('uploads.create');
   Route::post('/uploads/book', [UploadController::class, 'store'])->name('uploads.store');
-  Route::get('/uploads/book/{book_id}', [UploadController::class, 'edit'])->name('uploads.edit');
-  Route::patch('/uploads/book/{book_id}', [UploadController::class, 'update'])->name('uploads.update');
-  Route::delete('/uploads/book/{book_id}', [UploadController::class, 'destroy'])->name('uploads.destroy');
+  Route::get('/uploads/{book_id}', [UploadController::class, 'show'])->name('uploads.show');
+  Route::get('/uploads/{book_id}', [UploadController::class, 'edit'])->name('uploads.edit');
+  Route::patch('/uploads/{book_id}', [UploadController::class, 'update'])->name('uploads.update');
+  Route::delete('/uploads/{book_id}', [UploadController::class, 'destroy'])->name('uploads.destroy');
 
   Route::get('/uploads/{book_id}/chapters', [ChapterController::class, 'create'])->name('chapters.create');
   Route::post('/uploads/{book_id}/chapters', [ChapterController::class, 'store'])->name('chapters.store');
