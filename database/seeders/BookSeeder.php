146a5a1de4ac34846cbcdff7ba2620990d537170
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-12-04 16:18:09 +0800

    Add BookSeeder and update book display in Upload page

---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use Illuminate\Database\Seeder;
+
+class BookSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    // create 20 books for user with id 11
+    Book::factory()->count(20)->create([
+      'user_id' => 11,
+    ]);
+  }
+}
--- resources/js/Pages/Upload/Index.tsx
+++ resources/js/Pages/Upload/Index.tsx
@@ -33,18 +33,27 @@ const Index: FC<Props> = ({ bookData }) => {
 			{bookData.data.length === 0 ? (
 				<EmptyBooks message="No books uploaded yet." />
 			) : (
-				<div className="flex flex-wrap gap-2">
+				<div className="grid grid-cols-1 gap-x-2 gap-y-4 md:grid-cols-2 xl:grid-cols-3">
 					{bookData.data.map((book) => (
 						<div
 							key={book.id}
-							className="border"
+							className="flex"
 						>
-							<img
-								src={book.cover}
-								alt={book.title}
-								className="h-44 w-36 rounded-md border object-cover"
-							/>
-							<h1>{book.title}</h1>
+							<div className="overflow-hidden rounded-md">
+								<img
+									src={book.cover}
+									alt={book.title}
+									className="aspect-[3/4] w-36 object-cover transition-all hover:scale-125 sm:w-40"
+								/>
+							</div>
+							<div className="flex-1 space-y-2 px-4 py-2">
+								<h3 className="line-clamp-1 font-medium leading-none">
+									{book.title}
+								</h3>
+								<p className="line-clamp-2 text-sm text-muted-foreground">
+									{book.description}
+								</p>
+							</div>
 						</div>
 					))}
 				</div>
