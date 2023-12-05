ccf5be3bea8bfaa0bee28f6ef4ab40ba6fabb2d9
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2023-12-06 04:46:30 +0800

    Refactor Show component and add BookHeader, BookActions, BookTags, and ChapterList components

--- resources/js/Pages/Browse/Show.tsx
+++ resources/js/Pages/Browse/Show.tsx
@@ -1,6 +1,11 @@
-import { FC } from "react"
+import { FC, useMemo } from "react"
 
-import { Book } from "@/types"
+import { HeartIcon } from "lucide-react"
+
+import { EmptyState } from "@/Components/empty-state"
+import { Badge } from "@/Components/ui/badge"
+import { Button } from "@/Components/ui/button"
+import { Book, Chapter } from "@/types"
 
 interface Props {
 	book: Book
@@ -8,39 +13,119 @@ interface Props {
 
 const Show: FC<Props> = ({ book }) => {
 	return (
-		<>
+		<div className="relative">
+			<BackgroundImage
+				src={book.cover}
+				alt={book.title}
+			/>
 			<div className="relative">
-				<BackgroundImage
-					src={book.cover}
-					alt={book.title}
-				/>
-				<div className="flex space-x-4 border pt-5">
-					<img
-						src={book.cover}
-						alt={book.title}
-						className="aspect-[3/4] h-40 cursor-pointer select-none rounded bg-accent object-cover"
-					/>
-					<h1 className="text-2xl font-extrabold leading-6 tracking-wide drop-shadow-md [text-shadow:_0_2px_1px_rgb(0_0_0_/_20%)]">
-						{book.title}
-					</h1>
+				<BookHeader book={book} />
+				<div className="relative">
+					<div className="absolute h-full w-full scale-x-150 bg-background/80 backdrop-blur-md" />
+					<div className="relative pt-28">
+						<ChapterList chapters={book.chapters} />
+					</div>
 				</div>
 			</div>
-			<div>Chapters</div>
-		</>
+		</div>
 	)
 }
 
 const BackgroundImage = ({ src, alt }: { src: string; alt: string }) => {
 	return (
-		<div className="absolute -z-50 h-72 w-full scale-x-150">
+		<div>
 			<img
 				src={src}
 				alt={alt}
-				className="absolute inset-0 h-full w-full bg-accent object-cover"
+				className="h-72 w-full scale-150 bg-accent/80 object-cover"
 			/>
-			<div className="absolute inset-0 bg-gradient-to-b from-background/80 from-70% to-background"></div>
+			<div className="absolute inset-0 scale-150 bg-background/50 backdrop-blur-sm" />
 		</div>
 	)
+}
+
+const BookHeader = ({ book }: { book: Book }) => {
+	const tags = useMemo(
+		() => book.genres.split(",").map((genre) => genre.trim()),
+		[book.genres],
+	)
+
+	return (
+		<div className="-mb-12 -mt-64 flex space-x-4 sm:-mb-16 md:-mb-20 md:-mt-60 xl:-mb-24 xl:-mt-56 xl:space-x-8">
+			<img
+				src={book.cover}
+				alt={book.title}
+				className="z-10 aspect-[3/4] h-44 rounded border bg-slate-500 object-cover sm:h-52 md:h-60 lg:h-72 xl:h-80"
+			/>
+			<div className="z-10 flex flex-1 flex-col justify-between">
+				<div className="flex-1 space-y-2">
+					<h1 className="text-2xl font-black leading-7 lg:text-4xl lg:leading-none xl:text-5xl">
+						{book.title}
+					</h1>
+					<h2 className="text-sm sm:text-base md:text-lg lg:text-xl">
+						{book.author}
+					</h2>
+				</div>
+				<div className="flex flex-col gap-y-2">
+					<BookActions book={book} />
+					<BookTags tags={tags} />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+const BookActions = ({ book }: { book: Book }) => {
+	return (
+		<div className="flex items-center gap-x-2">
+			<Button className="h-12 px-10 text-lg">Read First</Button>
+			<Button
+				className="h-12 w-12"
+				variant={"secondary"}
+			>
+				<HeartIcon className="h-16 w-16" />
+			</Button>
+		</div>
+	)
+}
+
+const BookTags = ({ tags }: { tags: string[] }) => {
+	return (
+		<div className="flex flex-wrap gap-1">
+			{tags.map((tag) => (
+				<Badge
+					key={tag}
+					variant={"secondary"}
+					className="select-none bg-accent/60 text-muted-foreground"
+				>
+					{tag}
+				</Badge>
+			))}
+		</div>
+	)
+}
+
+const ChapterList = ({ chapters }: { chapters: Book["chapters"] }) => {
+	return (
+		<>
+			{chapters!.length === 0 ? (
+				<EmptyState message="No chapters uploaded yet." />
+			) : (
+				<div className="">
+					{chapters!.map((chapter) => (
+						<ChapterCard
+							key={chapter.id}
+							chapter={chapter}
+						/>
+					))}
+				</div>
+			)}
+		</>
+	)
+}
+
+const ChapterCard = ({ chapter }: { chapter: Chapter }) => {
+	return <div className="">{chapter.title}</div>
 }
 
 export default Show
