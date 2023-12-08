e1a0f07a63a1daaa08e97e8d85d057ac40a2c45d
Nekochan <115055895+kuro-Nekko@users.noreply.github.com>
2023-12-08 18:39:14 +0800

    space

--- database/seeders/ChapterSeeder.php
+++ database/seeders/ChapterSeeder.php
@@ -8,25 +8,25 @@ use App\Models\Chapter;
 
 class ChapterSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        $csvFile = fopen(resource_path("/data/chapters.csv"), "r");
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $csvFile = fopen(resource_path("/data/chapters.csv"), "r");
 
-        $firstline = true;
-        while (($data = fgetcsv($csvFile, 2000, ",")) !== FALSE) {
-            if (!$firstline) {
-                Chapter::create([
-                    'book_id' => $data[0],
-                    'title' => $data[1],
-                    'content' => $data[2],
-                ]);
-            }
-            $firstline = false;
-        }
-
-        fclose($csvFile);
+    $firstline = true;
+    while (($data = fgetcsv($csvFile, 2000, ",")) !== FALSE) {
+      if (!$firstline) {
+        Chapter::create([
+          'book_id' => $data[0],
+          'title' => $data[1],
+          'content' => $data[2],
+        ]);
+      }
+      $firstline = false;
     }
+
+    fclose($csvFile);
+  }
 }
